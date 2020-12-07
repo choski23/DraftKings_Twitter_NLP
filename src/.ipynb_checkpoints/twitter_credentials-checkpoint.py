f1d8224b1f196107ced49c48cfa6e8562893250b
choski23 <linda.m.cho.mil@gmail.com>
2020-12-07 11:19:09 -0500

    inital push

---
+++ src/.ipynb_checkpoints/twitter_credentials-checkpoint.py
@@ -0,0 +1,6 @@
+
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "2888838179-DPfnSZyVwbzIDoXOYOeFfsnA83ySwM2ApgRIHtA"
+ACCESS_TOKEN_SECRET = "jIw68X4pGlSV3cb58urKrL8mm7EobdtBf9d6XE6XS5MEg"
+CONSUMER_KEY = "Y6mRvbhFaZinGb3HMbaekWLk9"
+CONSUMER_SECRET = "R1IWmvvddzAwoJsB3iBogjm8dbncGfiPep5T56D05tZPfUUwBa"
